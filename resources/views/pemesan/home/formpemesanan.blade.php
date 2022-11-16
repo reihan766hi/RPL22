b377ec6497896e6108c00beca483faf0c2dc8785
daniellumbantobing <57679968+daniellumbantobing@users.noreply.github.com>
2022-11-16 14:01:54 +0700

    fix words in form pemesanan

--- resources/views/pemesan/home/formpemesanan.blade.php
+++ resources/views/pemesan/home/formpemesanan.blade.php
@@ -70,16 +70,13 @@
                     <div class="row align-items-center">
                         <div class="col-lg-7 mb-5 mb-lg-0">
                             <div class="mb-4">
-                                <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">Mega Offer</h6>
-                                <h1 class="text-white"><span class="text-primary">30% OFF</span> For Honeymoon</h1>
+                                <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">ShutleBus</h6>
+                                <h1 class="text-white"><span class="text-primary">Nikamti Perjalanan</span> Anda dengan ShutleBus</h1>
                             </div>
-                            <p class="text-white">Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo dolor lorem ipsum ut sed eos,
-                                ipsum et dolor kasd sit ea justo. Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est
-                                dolor</p>
                             <ul class="list-inline text-white m-0">
-                                <li class="py-2"><i class="fa fa-check text-primary mr-3"></i>Labore eos amet dolor amet diam</li>
-                                <li class="py-2"><i class="fa fa-check text-primary mr-3"></i>Etsea et sit dolor amet ipsum</li>
-                                <li class="py-2"><i class="fa fa-check text-primary mr-3"></i>Diam dolor diam elitripsum vero.</li>
+                                <li class="py-2"><i class="fa fa-check text-primary mr-3"></i>Nyaman</li>
+                                <li class="py-2"><i class="fa fa-check text-primary mr-3"></i>Praktis</li>
+                                <li class="py-2"><i class="fa fa-check text-primary mr-3"></i>Terjamin</li>
                             </ul>
                         </div>
                         <div class="col-lg-5">
